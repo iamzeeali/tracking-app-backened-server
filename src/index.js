b31b439981887ca393b5aed54e2cde542c87fae0
Zeeshan <mdzeeshanali93@gmail.com>
2020-03-24 15:29:28 +0530

    tracking-app-backened-server

---
+++ src/index.js
@@ -0,0 +1,37 @@
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri =
+  "mongodb+srv://iamzeeali:globus2015@glcluster-4yhn2.mongodb.net/track?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", err => {
+  console.error("Error connecting to mongo", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
